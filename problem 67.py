4b0da07f0d5ce62c8d02c53809247fc3cf7bba30
Jacques <dwamsie@gmail.com>
2023-02-23 13:34:28 -0500

    Initial Commit

--- problem 67.py
+++ problem 67.py
@@ -13,7 +13,7 @@ Find the maximum total from top to bottom in triangle.txt (right click and 'Save
 containing a triangle with one-hundred rows.
 
 NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem,
-as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty
+as there are 299 altogether! If you could check one trillion (10^12) routes every second it would take over twenty
 billion years to check them all. There is an efficient algorithm to solve it. ;o)
 """
 
