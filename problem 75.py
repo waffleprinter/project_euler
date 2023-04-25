f8fd19cd222f95cbfec4599cec6c54334d8d8775
waffleprinter <dwamsie@gmail.com>
2023-04-24 21:55:52 -0400

    Initial Commit

---
+++ problem 75.py
@@ -0,0 +1,38 @@
+"""
+It turns out that 12 cm is the smallest length of wire that can be bent to form an integer sided right angle triangle in
+exactly one way, but there are many more examples.
+
+12 cm: (3,4,5)
+24 cm: (6,8,10)
+30 cm: (5,12,13)
+36 cm: (9,12,15)
+40 cm: (8,15,17)
+48 cm: (12,16,20)
+
+In contrast, some lengths of wire, like 20 cm, cannot be bent to form an integer sided right angle triangle, and other
+lengths allow more than one solution to be found; for example, using 120 cm it is possible to form exactly three
+different integer sided right angle triangles.
+
+120 cm: (30,40,50), (20,48,52), (24,45,51)
+
+Given that L is the length of the wire, for how many values of L ≤ 1,500,000 can exactly one integer sided right angle
+triangle be formed?
+"""
+
+from math import gcd
+
+limit = 1500000
+length_frequencies = [0] * (limit + 1)
+
+for n in range(1, 1000):  # ONE THOUSAND IS AN ARBITRARY LIMIT, I COULDN'T BE BOTHERED TO CALCULATE IT
+    for m in range(n + 1, 1000, 2):
+        if gcd(m, n) != 1:
+            continue
+
+        length = 2 * m * (m + n)
+
+        if length <= limit:
+            for k in range(1, limit // length + 1):
+                length_frequencies[k * length] += 1
+
+print(length_frequencies.count(1))
